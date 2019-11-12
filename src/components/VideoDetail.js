feb346c65464e52e69cc3fb4599d456eb0a852b8
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-11 22:47:34 -0800

    Added iframe element to VideoDetail component. Updated some styling.

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -4,7 +4,20 @@ const VideoDetail = props => {
   if (!props.video) {
     return <div>Loading...</div>;
   }
-  return <div>{props.video.snippet.title}</div>;
+
+  const videoSrc = `https://www.youtube.com/embed/${props.video.id.videoId}`;
+
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe title="video player" src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4 className="ui header">{props.video.snippet.title}</h4>
+        <p>{props.video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
