b39d92506524403a3328b47e7c2301491efc423e
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-11 22:13:18 -0800

    Pulled out thumbnail and title attributes of response object for VideoList. Styled VideoList and VideoItem.

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = props => {
-  return <div>Video Item</div>;
+  return (
+    <div className="video-item item">
+      <img
+        className="ui image"
+        src={props.video.snippet.thumbnails.medium.url}
+      />
+      <div className="content">
+        <div className="header">{props.video.snippet.title}</div>
+      </div>
+    </div>
+  );
 };
 
 export default VideoItem;
