ae1d2aae2888a56f54bb84c3c596a62b6d9620f9
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-11 21:59:31 -0800

    Created VideoList and VideoItem components.

---
+++ src/components/VideoList.js
@@ -0,0 +1,11 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = props => {
+  const renderedList = props.videos.map(video => {
+    return <VideoItem />;
+  });
+  return <div>{renderedList}</div>;
+};
+
+export default VideoList;
